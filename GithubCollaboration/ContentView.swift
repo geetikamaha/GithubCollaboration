fd25877927ff2642e5d8697c259044817e3d54c1
Geetika Mahajan <geetika.mahajan16@gmail.com>
2025-05-09 14:10:46 -0700

    Initial Commit

---
+++ GithubCollaboration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubCollaboration
+//
+//  Created by Geetika Mahajan on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubCollaboration/GithubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubCollaborationApp.swift
+//  GithubCollaboration
+//
+//  Created by Geetika Mahajan on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
